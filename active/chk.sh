5a5a42ae72db1c2a678c5a3258dbe47064e360fb
Hiroki UEMATSU <uematsu.hiroki@gmail.ocm>
2014-12-22 16:14:10 +0900

    modified to check smsExecute process running.

--- active/chk.sh
+++ active/chk.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-TARGET_DIR=/home/pi/csn_raspi/active
+BASE_DIR=/home/pi/csn_raspi
+TARGET_DIR=${BASE_DIR}/active
+
 LASTTIME=`date +'%s'`
+
+# process check
+EXEC_CMD=${BASE_DIR}/executeGetData/localExecute
+IS_RUN=`ps -ef | grep ${EXEC_CMD} | grep -v | wc -l`
+if [ ${IS_RUN} = 0 ]; then
+  ${BASE_DIR}/tools/runSMSExecute.sh
+else
+fi
 
 START=${TARGET_DIR}/start.txt
 ALIVE=${TARGET_DIR}/alive.txt
---
+++ tools/runSMSExecute.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BASE_DIR=/home/pi/csn_raspi/executeGetData
+# process check
+EXEC_CMD=${BASE_DIR}/localExecute
+LOG_DIR=${BASE_DIR}/logs
+IS_RUN=`ps -ef | grep ${EXEC_CMD} | grep -v | wc -l`
+if [ ${IS_RUN} = 0 ]; then
+  nohup ${EXEC_CMD} > ${LOG_DIR}/out.log 2> ${LOG_DIR}/err.log < /dev/null &
+else
+  #echo "alerady run"
+fi
