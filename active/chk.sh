6935c2f51e73a39a919442d5e1c0c477a398a715
Hiroki UEMATSU <uematsu.hiroki@gmail.com>
2014-12-22 16:49:28 +0900

    bug fixed

--- active/chk.sh
+++ active/chk.sh
@@ -7,7 +7,7 @@ LASTTIME=`date +'%s'`
 
 # process check
 EXEC_CMD=${BASE_DIR}/executeGetData/localExecute
-IS_RUN=`ps -ef | grep ${EXEC_CMD} | grep -v | wc -l`
+IS_RUN=`ps -ef | grep ${EXEC_CMD} | grep -v grep | wc -l`
 if [ ${IS_RUN} = 0 ]; then
   ${BASE_DIR}/tools/runSMSExecute.sh
 fi
--- tools/runSMSExecute.sh
+++ tools/runSMSExecute.sh
@@ -4,7 +4,8 @@ BASE_DIR=/home/pi/csn_raspi/executeGetData
 # process check
 EXEC_CMD=${BASE_DIR}/localExecute
 LOG_DIR=${BASE_DIR}/logs
-IS_RUN=`ps -ef | grep ${EXEC_CMD} | grep -v | wc -l`
+IS_RUN=`ps -ef | grep ${EXEC_CMD} | grep -v grep | wc -l`
 if [ ${IS_RUN} = 0 ]; then
   nohup ${EXEC_CMD} > ${LOG_DIR}/out.log 2> ${LOG_DIR}/err.log < /dev/null &
+  #${EXEC_CMD} > ${LOG_DIR}/out.log 2> ${LOG_DIR}/err.log < /dev/null &
 fi
