013e9695efd21bcd8318d24e5eee429d051842bf
Hiroki UEMATSU <uematsu.hiroki@gmail.ocm>
2014-12-23 19:25:06 +0900

    compress dumped files

--- tools/dailyDump.sh
+++ tools/dailyDump.sh
@@ -10,8 +10,11 @@ LASTDATE=`date -d "${LASTDATE}" +"%Y%m%d%I"`
 DB_NAME=csn
 TABLE=Event
 
+DUMP_FILE=${TARGET_DIR}/data_${LASTDATE}.sql
+
 # every hour
-mysqldump -u root ${DB_NAME} ${TABLE} --where="t0active < ${LASTDATE_TIME}" > ${TARGET_DIR}/data_${LASTDATE}.sql
+mysqldump -u root ${DB_NAME} ${TABLE} --where="t0active < ${LASTDATE_TIME}" > ${DUMP_FILE}
+bzip2 ${DUMP_FILE}
 
 # delete
 mysql -u root ${DB_NAME} -e "DELETE FROM Event WHERE t0active < ${LASTDATE_TIME}"
